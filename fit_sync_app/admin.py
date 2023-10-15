9414a874596575d6c8f12349738d2c963f3f46d4
Bartek Burnat <bartekmtb@gmail.com>
2023-10-15 13:48:00 +0100

    create ne project

--- fit_sync_app/admin.py
+++ fit_sync_app/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import Scheduled_Lessons_Main
+
 # Register your models here.
-
-
-admin.site.register(Scheduled_Lessons_Main)
--- fit_sync_app/models.py
+++ fit_sync_app/models.py
@@ -1,7 +1,3 @@
-from django.db import models 
-from django.contrib.auth.models import User
+from django.db import models
 
-class Scheduled_Lessons_Main(models.Model):
-    created_for_user = models.OneToOneField(User, on_delete=models.CASCADE)
-    type_of_lesson = models.TextChoices("Schedule Lesson","Online Lesson")
-    time_and_date = models.DateTimeField()
+# Create your models here.
