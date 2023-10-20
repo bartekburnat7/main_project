30c3134b9f154e85abd0b207fa6461b5cb1c8a43
Bartek Burnat <bartekmtb@gmail.com>
2023-10-20 14:18:09 +0100

    create a form that querys user model and inputs data from the form.

--- fit_sync_app/models.py
+++ fit_sync_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class schedule_of_lesson(models.Model):
-    created_by_user = models.OneToOneField(User, on_delete=models.CASCADE, blank=False)
+    created_by_user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE, related_name='created_by_user')
+    created_for_user = models.CharField(max_length=256, blank=False)
     type_of_lesson = models.CharField(max_length=200, blank=False)
     time_of_lesson = models.DateTimeField(blank=False)
--- fit_sync_app/views.py
+++ fit_sync_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib import messages
+from fit_sync_app.models import schedule_of_lesson
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def schedule(request):
+    if request.method == 'POST':
+        # created_by_user = request.POST.get('created_by_user')
+        current_user = request.user
+        created_for_user = request.POST.get('created_for_user')
+        type_of_lesson = request.POST.get('type_of_lesson')
+        time_of_lesson = request.POST.get('time_of_lesson')
+        try:
+            form_for_user = User.objects.get(username=created_for_user)
+            schedule_of_lesson.objects.create(created_by_user=current_user, created_for_user=form_for_user, type_of_lesson=type_of_lesson, time_of_lesson=time_of_lesson)
+        except User.DoesNotExist:
+            messages.error(request, 'User does not exist')            
+    return render(request, "schedule.html")
