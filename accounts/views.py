5e6167bdf28b9e37c90756e84ed055a4f30f11eb
Bartek Burnat <bartekmtb@gmail.com>
2023-11-27 18:34:52 +0000

    Add form errors to my custom user signup form

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth import login
 from .forms import SignupForm
+from fit_sync_app import views
+from accounts.models import CustomUser
 
 def signup(request):
     if request.user.is_authenticated:
-        return redirect("dashboard")   
+        return redirect("dashboard")
+    
     if request.method == 'POST':
         form = SignupForm(request.POST)
-        print(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('home')
     else:
         form = SignupForm()
-    return render(request, "signup.html")
+    return render(request, "signup.html", {'form': form.errors})
